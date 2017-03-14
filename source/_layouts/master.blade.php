dec30641810c0456e3888338ec95970f010b1750
Steve Schoger <sm.schoger@gmail.com>
2017-03-14 14:06:37 -0400

    Updated title

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -7,6 +7,7 @@
         <link rel="stylesheet" href="/css/main.css">
         <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/styles/default.min.css">
         <link type="text/css" rel="stylesheet" href="//fast.fonts.net/cssapi/10f37207-0e28-4555-b7b4-e70a13d03463.css"/>
+        <title>Steve Schoger</title>
     </head>
     <body>
         @yield('body')
