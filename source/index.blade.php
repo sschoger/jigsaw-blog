114c12b03483992cd8747598b816b7b6fbc5f861
Steve Schoger <sm.schoger@gmail.com>
2017-03-14 10:30:23 -0400

    Status code link

--- source/index.blade.php
+++ source/index.blade.php
@@ -34,7 +34,7 @@
                 <ul class="projects no-list-style">
                     <li><a href="http://www.zondicons.com/">Zondicons<svg width="12" height="7" viewBox="0 0 12 7" xmlns="http://www.w3.org/2000/svg"><path class="icon-link" d="M9.293 3H0v1h9.293L7.146 6.146l.708.708L11.207 3.5 7.854.146l-.708.708L9.293 3" fill-rule="evenodd"/></svg></a></li>
                     <li><a href="http://www.heropatterns.com/">Hero Patterns<svg width="12" height="7" viewBox="0 0 12 7" xmlns="http://www.w3.org/2000/svg"><path class="icon-link" d="M9.293 3H0v1h9.293L7.146 6.146l.708.708L11.207 3.5 7.854.146l-.708.708L9.293 3" fill-rule="evenodd"/></svg></a></li>
-                    <li><a href="/status-code-poster/status-code-poster/">HTTP Status Code Poster<svg width="12" height="7" viewBox="0 0 12 7" xmlns="http://www.w3.org/2000/svg"><path class="icon-link" d="M9.293 3H0v1h9.293L7.146 6.146l.708.708L11.207 3.5 7.854.146l-.708.708L9.293 3" fill-rule="evenodd"/></svg></a></li>
+                    <li><a href="status-code-poster/status-code-poster/">HTTP Status Code Poster<svg width="12" height="7" viewBox="0 0 12 7" xmlns="http://www.w3.org/2000/svg"><path class="icon-link" d="M9.293 3H0v1h9.293L7.146 6.146l.708.708L11.207 3.5 7.854.146l-.708.708L9.293 3" fill-rule="evenodd"/></svg></a></li>
                     <li><a href="http://www.heroicons.com">Heroicons<svg width="12" height="7" viewBox="0 0 12 7" xmlns="http://www.w3.org/2000/svg"><path class="icon-link" d="M9.293 3H0v1h9.293L7.146 6.146l.708.708L11.207 3.5 7.854.146l-.708.708L9.293 3" fill-rule="evenodd"/></svg></a></li>
                 </ul>
             </div>
