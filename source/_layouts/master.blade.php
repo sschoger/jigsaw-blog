e56790d649d920f710ec04c82bc11486e3d03ad3
Steve Schoger <sm.schoger@gmail.com>
2017-04-20 09:10:28 -0400

    Google Analytics

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -8,6 +8,16 @@
         <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/styles/default.min.css">
         <link type="text/css" rel="stylesheet" href="//fast.fonts.net/cssapi/10f37207-0e28-4555-b7b4-e70a13d03463.css"/>
         <title>Steve Schoger</title>
+        <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-15245582-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
     </head>
     <body>
         @yield('body')
